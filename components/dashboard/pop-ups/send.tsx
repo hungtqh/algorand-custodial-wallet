48fab50089f764dfb2b7ea98b61277832a43f719
soheil <soheilrasekh555@gmail.com>
2021-12-27 11:20:09 +0330

    add sending functionality

--- components/dashboard/pop-ups/send.tsx
+++ components/dashboard/pop-ups/send.tsx
@@ -1,12 +1,33 @@
 import { useSelector } from "react-redux";
 import { RootState } from "redux/store";
+import { FormikProps, Form, Field, withFormik } from "formik";
+import axios from "axios";
+import * as Yup from "yup";
+
+//TODO: check if address is valid
+//TODO: check max amount
 
 type Props = {
   setSendActive: (state: boolean) => void;
 };
 
+type SendAlgoProps = {
+  amount: number;
+  address: string;
+};
+
+type FormProps = {};
+
 export default function Send({ setSendActive }: Props) {
   const { currentWallet } = useSelector((state: RootState) => state.customer);
+
+  const SendAlgoSchema = Yup.object().shape({
+    amount: Yup.number()
+      .required("amount is required")
+      .moreThan(0, "amount must be greater than 0")
+      .lessThan(currentWallet.balance, "must be less than wallet balance"),
+    address: Yup.string().required("address is required"),
+  });
 
   const handleExit = (e: React.MouseEvent<HTMLDivElement>) => {
     const target = e.target as HTMLDivElement;
@@ -16,15 +37,90 @@ export default function Send({ setSendActive }: Props) {
     }
   };
 
+  const InnerForm = (props: FormikProps<SendAlgoProps>) => {
+    const { touched, errors, isSubmitting, status } = props;
+
+    return (
+      <Form className="bg-white w-[30rem] h-[30rem] rounded-3xl flex flex-col justify-around items-center">
+        <h3>Receive</h3>
+
+        <div className="w-[95%]">
+          <p className="mb-2">Amount</p>
+          <div className="border-2 border-sky-300 rounded-md flex h-14">
+            <div className="w-[40%] bg-sky-100 mr-2 flex justify-around items-center">
+              <p>balance</p>
+              <p>{currentWallet.balance}</p>
+            </div>
+            <Field
+              className="border-none p-0 w-[70%] focus:ring-0"
+              placeholder="0.0"
+              type="number"
+              name="amount"
+            />
+          </div>{" "}
+          {touched.amount && errors.amount && (
+            <div className="text-red-400">{errors.amount}</div>
+          )}
+        </div>
+
+        <div className="w-[95%]">
+          <p className="mb-2">Address</p>
+          <Field
+            type="text"
+            name="address"
+            className="py-4 px-2 border-2 rounded-md flex items-center justify-between "
+          />
+          {touched.address && errors.address && (
+            <div className="text-red-400">{errors.address}</div>
+          )}
+        </div>
+
+        <button
+          type="submit"
+          className="px-20 py-2 rounded-lg bg-sky-600 text-white disabled:bg-gray-200 disabled:text-gray-500 disabled:cursor-not-allowed"
+          disabled={
+            (Object.keys(errors).length !== 0 ||
+              Object.keys(touched).length !== 2) &&
+            !isSubmitting
+          }
+        >
+          {isSubmitting ? "Sending..." : "Send"}
+        </button>
+        {status && <p className="text-center text-red-400">{status}</p>}
+      </Form>
+    );
+  };
+
+  const SendAlgoForm = withFormik<FormProps, SendAlgoProps>({
+    mapPropsToValues: () => {
+      return {
+        amount: 0,
+        address: "",
+      };
+    },
+    validationSchema: SendAlgoSchema,
+    handleSubmit: async ({ address, amount }, { setStatus }) => {
+      try {
+        await axios.post("/api/wallet/transaction", {
+          from: currentWallet.address,
+          to: address,
+          amount,
+        });
+        setSendActive(false);
+      } catch (error) {
+        if (error.response) {
+          setStatus(error.response.data.error);
+        }
+      }
+    },
+  })(InnerForm);
+
   return (
     <div
       onClick={handleExit}
       className="pop-up fixed top-0 left-0 w-full h-full z-20 bg-black bg-opacity-50 flex items-center justify-center"
     >
-      <div className="bg-white w-[25rem] h-[25rem] rounded-md flex flex-col justify-around items-center">
-        <h2>Wallet address</h2>
-        {currentWallet && ""}
-      </div>
+      <SendAlgoForm />
     </div>
   );
 }
---
+++ pages/api/wallet/transaction/index.ts
@@ -0,0 +1,69 @@
+import { withSessionRoute } from "lib/withSession";
+import { NextApiRequest, NextApiResponse } from "next";
+import axios from "axios";
+import * as Yup from "yup";
+import prisma from "lib/prisma";
+
+const schema = Yup.object().shape({
+  from: Yup.string().required("from is required"),
+  to: Yup.string().required("to is required"),
+  amount: Yup.number().required("amount is required"),
+});
+
+//TODO: find it on sdk
+export default withSessionRoute(transactionRoute);
+
+async function transactionRoute(req: NextApiRequest, res: NextApiResponse) {
+  const {
+    method,
+    body,
+    session: { user },
+  } = req;
+
+  switch (method) {
+    case "POST":
+      if (!user) {
+        return res.status(401).end("unauthorized");
+      }
+
+      const data = await schema.validate(body);
+
+      const fromWallet = await prisma.wallet.findFirst({
+        where: { address: data.from, ownerId: user.id },
+      });
+
+      if (!fromWallet) {
+        return res.status(403).send({ error: "wallet is not belongs to user" });
+      }
+
+      const amount = String(data.amount);
+
+      try {
+        const result = await axios.post(
+          "https://api-eu1.tatum.io/v3/algorand/transaction",
+          {
+            from: fromWallet.address,
+            to: data.to,
+            amount: amount,
+            fee: "0.001",
+            fromPrivateKey: fromWallet.secret,
+          },
+          {
+            headers: {
+              "content-type": "application/json",
+              "x-api-key": process.env.TATUM_API_KEY!,
+            },
+          }
+        );
+        return res.status(200).json(result.data);
+      } catch (error) {
+        console.error(error);
+        const data = error.response.data;
+        return res.status(data.statusCode).json({ error: data.message });
+      }
+
+    default:
+      res.setHeader("Allow", ["POST"]);
+      res.status(405).end(`Method ${method} is not allowed`);
+  }
+}
