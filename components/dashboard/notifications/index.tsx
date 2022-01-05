f01cb4b93f3071d94ba768646ff21fa85330b29a
soheil <soheilrasekh555@gmail.com>
2022-01-05 16:41:32 +0330

    fix notification width on mobile view

--- components/dashboard/notifications/index.tsx
+++ components/dashboard/notifications/index.tsx
@@ -24,7 +24,7 @@ export default function Notifications() {
   }, [notifications]);
 
   return show ? (
-    <div className="absolute bottom-5 right-5 w-[50%] md:w-[30%] flex justify-end">
+    <div className="absolute bottom-5 right-5 w-[20rem] md:w-[25rem] lg:w-[30rem] flex justify-end">
       <Notification
         setShow={setShow}
         type={alert.type}
