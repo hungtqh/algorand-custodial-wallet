1e73fb60dff2500d0ede595ce558c0d3385e88c0
soheil <soheilrasekh555@gmail.com>
2022-01-06 15:12:57 +0330

    fix router push bug

--- components/user/signout.tsx
+++ components/user/signout.tsx
@@ -1,10 +1,19 @@
 import axios from "axios";
 import Router from "next/router";
+import { useEffect } from "react";
 
 export default function Signout() {
-  axios.get("/api/user/logout").then(() => {
-    Router.push("/signin");
-  });
+
+  useEffect(()=> {
+
+    axios.get("/api/user/logout").then(() => {
+      Router.push("/signin");
+    });
+
+
+  },[])
+
+
 
   return <div>Sign out...</div>;
 }
